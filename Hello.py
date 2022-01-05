634952c4d7f323b4d8b37d12b33d3bc1203a5c9a
Sarveshkapse123 <67366485+Sarveshkapse123@users.noreply.github.com>
2022-01-05 11:56:07 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,9 @@
+def computepay(h,r) :
+    if h <= 40:
+       return h * r
+    if h > 40:
+      return (h *r) + ((h-40)*(r/2))
+h = float(input('Total Hours :'))
+r = float(input('Rate Per Hour :'))
+P = computepay(h,r)
+print('Gross Pay' ,P)
